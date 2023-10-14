c2e8fdb9c619936c21053aba7c77f5ad2b2c06b6
bithighlander <bithighlander@gmail.com>
2023-10-14 16:53:07 -0500

    type debugging

--- src/lib/layout/Header.tsx
+++ src/lib/layout/Header.tsx
@@ -1,22 +1,37 @@
-import { Box, Flex } from "@chakra-ui/react";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { Box, Flex, HStack, Text } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
 
-import ThemeToggle from "./ThemeToggle";
+// import { Pioneer }  from "@pioneer-sdk/pioneer-react";
 
-const Header = () => {
+const PROJECT_NAME = "*Your Project name here*";
+// eslint-disable-next-line no-console
+const HeaderNew = () => {
   return (
     <Flex
       as="header"
       width="full"
-      align="center"
       alignSelf="flex-start"
-      justifyContent="center"
       gridGap={2}
+      justifyContent="space-between"
+      alignItems="center"
+      p={5}
+      bg="gray.900" // change background color
+      borderBottom="1px solid" // add a border at the bottom
+      borderColor="gray.200" // set border color
     >
-      <Box marginLeft="auto">
-        <ThemeToggle />
-      </Box>
+      <HStack spacing={8}>
+        <RouterLink to="/">
+          <Box>
+            <Text fontSize="3xl">{PROJECT_NAME}</Text>
+          </Box>
+        </RouterLink>
+      </HStack>
+      <br />
+      {/* <Pioneer /> */}
     </Flex>
   );
 };
 
-export default Header;
+export default HeaderNew;
--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -1,10 +1,142 @@
-import { Grid } from "@chakra-ui/react";
+import {
+  Button,
+  useDisclosure,
+  Modal,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+} from "@chakra-ui/react";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { usePioneer } from "@pioneer-sdk/pioneer-react";
+import { useEffect, useState } from "react";
+// import AssetSelect from "lib/components/AssetSelect";
+// import OutputSelect from "lib/components/OutputSelect";
+// import BlockchainSelect from "lib/components/BlockchainSelect";
+// import WalletSelect from "lib/components/WalletSelect";
+// import Basic from "./components/Basic";
+// import Balances from "./components/Balances";
+// // import Pubkeys from "./components/Pubkeys";
+// import Transfer from "./components/Transfer";
+// import Swap from "./components/Swap";
 
 const Home = () => {
+  const { state } = usePioneer();
+  const {
+    // api,
+    // app,
+    // context,
+    // assetContext,
+    // blockchainContext,
+    pubkeyContext,
+    // modals,
+  } = state;
+  const [address, setAddress] = useState("");
+  const [modalType] = useState("");
+  const { isOpen, onClose } = useDisclosure();
+
+  useEffect(() => {
+    if (pubkeyContext)
+      setAddress(
+        pubkeyContext?.master || pubkeyContext?.pubkey || pubkeyContext
+      );
+  }, [pubkeyContext]);
+
+  // const openModal = (type: any) => {
+  //   setModalType(type);
+  //   onOpen();
+  // };
+
+  const refresh = async () => {
+    // TODO why do I need to press refresh?
+    // console.log("2 pubkeyContext: ", pubkeyContext);
+    // console.log("2 balances: ", app.balances);
+    if (pubkeyContext)
+      setAddress(
+        pubkeyContext?.master || pubkeyContext?.pubkey || pubkeyContext
+      );
+    // console.log("pubkeyContext: ", pubkeyContext);
+  };
+
   return (
-    <Grid gap={4}>
-      <h1>Home</h1>
-    </Grid>
+    <div>
+      <Modal isOpen={isOpen} onClose={() => onClose()} size="xl">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{modalType}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {/* Render content based on modalType */}
+            {/* {modalType === "Select wallet" && ( */}
+            {/*  <div> */}
+            {/*    <WalletSelect onClose={onClose}></WalletSelect> */}
+            {/*  </div> */}
+            {/* )} */}
+            {/* {modalType === "Select Asset" && ( */}
+            {/*  <div> */}
+            {/*    <AssetSelect onClose={onClose} onlyOwned={true}></AssetSelect> */}
+            {/*  </div> */}
+            {/* )} */}
+            {/* {modalType === "Select Blockchain" && ( */}
+            {/*  <div> */}
+            {/*    <BlockchainSelect onClose={onClose}></BlockchainSelect> */}
+            {/*  </div> */}
+            {/* )} */}
+            {/* {modalType === "View Address" && ( */}
+            {/*  <div> */}
+            {/*    {JSON.stringify(pubkeyContext)} address: {address} */}
+            {/*  </div> */}
+            {/* )} */}
+            {/* {modalType === "Select Outbound" && ( */}
+            {/*  <div> */}
+            {/*    <OutputSelect onClose={onClose} onlyOwned={false}></OutputSelect> */}
+            {/*  </div> */}
+            {/* )} */}
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      {address}
+      <Tabs>
+        <TabList>
+          <Tab>Context</Tab>
+          <Tab>balances</Tab>
+          <Tab>Transfer</Tab>
+          <Tab>Swaps</Tab>
+          <Tab>Earn</Tab>
+          <Tab>Borrow</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>{/* <Basic></Basic> */}</TabPanel>
+          <TabPanel>
+            {/* <Balances openModal={openModal}></Balances> */}
+          </TabPanel>
+          <TabPanel>
+            {/* <Transfer openModal={openModal}></Transfer> */}
+          </TabPanel>
+          <TabPanel>{/* <Swap openModal={openModal}></Swap> */}</TabPanel>
+          <TabPanel>
+            <p>Earn</p>
+          </TabPanel>
+          <TabPanel>
+            <p>Borrow</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+      <Button onClick={refresh}>refresh</Button>
+    </div>
   );
 };
 
